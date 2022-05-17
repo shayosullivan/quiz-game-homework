764e5b75df0e1ae71296366dfeba589959553320
shay <shayosullivan@hotmail.com>
2022-05-17 07:58:34 -0400

    start button added and working

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,13 @@
+const startButton = document.getElementById("start-btn");
+const questionContainerElement = document.getElementById("question-container");
+
+startButton.addEventListener("click", startGame);
+function startGame() {
+  console.log("Started");
+  startButton.classList.add("hide");
+  questionContainerElement.classList.remove("hide");
+}
+
 function Quiz(questions) {
   this.score = 0;
   this.questions = questions;
